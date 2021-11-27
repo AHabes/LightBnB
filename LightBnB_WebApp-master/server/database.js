8e272b5edc7d2c90f8e6731a2a3ac8aa3fdbab72
Abdullah Habes <abdulla096@hotmail.com>
2021-11-27 00:00:09 -0500

    update getUserWithId to use the lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -33,7 +33,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT *
+                     FROM users
+                     WHERE id = $1`, [id])
+    .then(user =>  user.rows[0])
+    .catch(err => console.log(err));
 };
 exports.getUserWithId = getUserWithId;
 
